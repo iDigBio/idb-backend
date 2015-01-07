280733b4796674d240c8cb9eb00b4c1e7a48fae0
Alex Thompson <godfoder@acis.ufl.edu>
2015-01-07 11:28:42 -0500

    self ref

--- redis_backend/queue.py
+++ redis_backend/queue.py
@@ -6,22 +6,23 @@ class RedisQueue(object):
 
     def __init__(queue_prefix=""):
         self.p = redist.pubsub()
+        self.queue_prefix = queue_prefix
 
 
     def listen(sleep_time=1):
-        self.p.psubscribe(queue_prefix + "*")
+        self.p.psubscribe(self.queue_prefix + "*")
 
         count = 0
         for message in p.listen():
-            t = message["channel"][len(queue_prefix):]
+            t = message["channel"][len(self.queue_prefix):]
             if message["data"] == "shutdown":
                 break
-            e = redist.spop(queue_prefix + t + "_queue")
+            e = redist.spop(self.queue_prefix + t + "_queue")
             if e is not None:
                 yield (t,e)
             else:
                 time.sleep(sleep_time)
 
     def add(t,e):
-        redist.sadd(queue_prefix + t + "_queue",e)
-        redist.publish(queue_prefix + t, e)
+        redist.sadd(self.queue_prefix + t + "_queue",e)
+        redist.publish(self.queue_prefix + t, e)

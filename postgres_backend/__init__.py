26d7637f62a1cd7826aebc26e64e9c3d98eda78a
Alex Thompson <godfoder@acis.ufl.edu>
2015-04-15 11:51:10 -0400

    switch over to new database name, back to config for properties.

--- postgres_backend/__init__.py
+++ postgres_backend/__init__.py
@@ -1,17 +1,21 @@
 import psycopg2
 import os
+import copy
 from psycopg2.extras import DictCursor
 
 from config import config
 
-# pg_conf = config["postgres"]
-# pg_conf["database"] = "config["postgres"]["db_prefix"] + 'data_cache'"
-# del pg_conf["db_prefix"]
+prefix = config["postgres"]["db_prefix"] if "db_prefix" in config["postgres"] else ""
+suffix = config["postgres"]["db_suffix"] if "db_suffix" in config["postgres"] else ""
 
-# pg = psycopg2.connect(**pg_conf)
+pg_conf = copy.deepcopy(config["postgres"])
+del pg_conf["db_prefix"]
+del pg_conf["db_suffix"]
+
+pg_conf["database"] = prefix + 'api' + suffix
 
 pg = None
 if os.environ["ENV"] == "test":
     pg = psycopg2.connect(host="localhost",user="test",password="test",dbname="test")
 else:
-    pg = psycopg2.connect(host="c18node16.acis.ufl.edu",user="idigbio",password="idigbiotest",dbname="idb-prod-new")
+    pg = psycopg2.connect(**pg_conf)

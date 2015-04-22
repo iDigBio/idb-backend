c2a4c6080bda58b4b5d4a15e2316d9bb296a88b7
Alex Thompson <godfoder@acis.ufl.edu>
2015-04-22 10:40:19 -0400

    working on new taxon code.

--- idb/data_tables/taxon.py
+++ idb/data_tables/taxon.py
@@ -1,3 +1,5 @@
+import traceback
+
 from idb.lib.data.importers.dwca import Dwca
 
 whitelist = [
@@ -11,8 +13,9 @@ whitelist = [
     #"vernacularname.txt"
 ]
 
+
 def get_data(path_to_checklist):
-    d = Dwca(path_to_checklist,logname="idigbio")
+    d = Dwca(path_to_checklist, logname="idigbio")
 
     sp_count = 0
     sp_non_count = 0
@@ -21,7 +24,8 @@ def get_data(path_to_checklist):
     ids = {}
     needs_name = []
     taxa_lines = []
-    higher_taxons = ["dwc:kingdom","dwc:phylum","dwc:class","dwc:order","dwc:superfamily"]
+    higher_taxons = ["dwc:kingdom", "dwc:phylum",
+                     "dwc:class", "dwc:order", "dwc:superfamily"]
     for r in d.core:
         try:
             if r["dwc:taxonRank"] == "family":
@@ -30,8 +34,9 @@ def get_data(path_to_checklist):
                     skiprest = False
                     for ht in higher_taxons:
                         if ht in r:
-                            hta[ht] = r[ht]
-                    taxa_lines.append(({"dwc:family": r["gbif:canonicalName"]},hta,"gbif_checklist",True))
+                            hta[ht] = r[ht].lower().split(" ")[0]
+                    taxa_lines.append(
+                        ({"dwc:family": r["gbif:canonicalName"]}, hta, "gbif_checklist", True))
             if r["dwc:taxonRank"] == "species":
                 if "dwc:genus" in r and "dwc:specificEpithet" in r:
                     g = r["dwc:genus"].lower()
@@ -40,23 +45,18 @@ def get_data(path_to_checklist):
 
                     if r["dwc:taxonomicStatus"] == "accepted":
                         sp_count += 1
-                        ids[id] = (g,s)
-                        gbif_t = {
-                            "gbif:genus": g,
-                            "gbif:specificEpithet": s,
-                            "gbif:taxonID": id,
-                            "gbif:cannonicalName": r["gbif:canonicalName"].lower()
-                        }
-                        for ht in higher_taxons + ["dwc:family"]:
-                            if ht in r:
-                                gbif_t[ht] = r[ht]
-                        names[(g,s)] = gbif_t
+                        ids[id] = (g, s)
+                        if (g, s) not in names:
+                            names[(g, s)] = []
+                        names[(g, s)].append(r)
                     elif "dwc:acceptedNameUsageID" in r:
                         sp_non_count += 1
                         if r["dwc:acceptedNameUsageID"] in ids:
-                            names[(g,s)] = names[ids[r["dwc:acceptedNameUsageID"]]]
+                            names[(g, s)] = names[
+                                ids[r["dwc:acceptedNameUsageID"]]]
                         else:
-                            needs_name.append((g,s,r["dwc:acceptedNameUsageID"]))
+                            needs_name.append(
+                                (g, s, r["dwc:acceptedNameUsageID"]))
                     else:
                         fail += 1
                 else:
@@ -64,29 +64,63 @@ def get_data(path_to_checklist):
         except:
             print r
 
-    #print len(names), len(needs_name)
+    print len(names), len(needs_name)
     total_miss = 0
-    for g,s,id in needs_name:
+    for g, s, id in needs_name:
         if id in ids:
-            names[(g,s)] = names[ids[id]]
+            if (g, s) not in names:
+                names[(g, s)] = []
+            names[(g, s)].extend(names[ids[id]])
         else:
             total_miss += 1
 
-    #print sp_count, sp_non_count, fail, total_miss
+    print sp_count, sp_non_count, fail, total_miss
 
-    for g,s in names:
-        taxa_lines.append(({"dwc:genus": g, "dwc:specificEpithet": s},names[(g,s)],"gbif_checklist_gs",True))
+    multiname = 0
+    linefail = 0
+    for g, s in names:
+        if len(names[(g, s)]) > 1:
+            multiname += 1
+
+        gibf_t = {}
+        for n in names[(g, s)]:
+            try:
+                gbif_t.update({
+                    "gbif:genus": g,
+                    "gbif:specificEpithet": s,
+                    "gbif:taxonID": n["id"],
+                    "gbif:cannonicalName": n["gbif:canonicalName"].lower()
+                })
+                for ht in higher_taxons + ["dwc:family"]:
+                    if ht in n:
+                        if ht in gbif_t and gbif_t[ht] != n[ht].lower().split(" ")[0]:
+                            print ht, names[(g, s)]
+                        gbif_t[ht] = n[ht].lower().split(" ")[0]
+            except:
+                traceback.print_exc()
+                print n
+                linefail += 1
+        taxa_lines.append((
+            {"dwc:genus": g, "dwc:specificEpithet": s},
+            gbif_t,
+            "gbif_checklist_gs",
+            True
+        ))
+
+    print multiname, linefail
 
     return taxa_lines
 
+
 def get_sources():
-    return ["gbif_checklist","gbif_checklist_gs"]
+    return ["gbif_checklist", "gbif_checklist_gs"]
+
 
 def main():
     count = 0
     for dr in get_data("/home/godfoder/taxon/checklist1.zip"):
         count += 1
-        #print dr
+        # print dr
     print count
 
 if __name__ == '__main__':

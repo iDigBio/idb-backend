9080c4eac3afa76ec0461605efcc21b781cb8817
Alex Thompson <godfoder@acis.ufl.edu>
2015-07-07 14:58:43 -0400

    gevent data api, missing requirement.

--- idb/start_data_api.py
+++ idb/start_data_api.py
@@ -1,3 +1,10 @@
 if __name__ == '__main__':
+    from gevent.pool import Pool
+    from gevent.wsgi import WSGIServer
     from data_api import app
-    app.run(debug=True)
+
+    print "Server Start"
+    http_server = WSGIServer(('', 19197), app, spawn=Pool(1000))
+    http_server.serve_forever()
+
+    #app.run(debug=True)

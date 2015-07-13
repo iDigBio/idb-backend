005276f55f904713eebcf50bcbf2d33fbc9fd7f0
Alex Thompson <godfoder@acis.ufl.edu>
2015-07-13 10:46:58 -0400

    switch to using the api, so we only get recordsets set to ingest.

--- idigbio_ingestion/db_rsids.py
+++ idigbio_ingestion/db_rsids.py
@@ -1,13 +1,13 @@
-from idb.postgres_backend.db import PostgresDB
+import requests
+import json
 
 def main():
-    db = PostgresDB()
-    for r in db.get_type_list("recordset", limit=None):
-        try:
-            print r["uuid"]
-        except:
-            traceback.print_exc()
+    r = requests.get("http://search.idigbio.org/v2/search/recordsets/", params={"rsq":json.dumps({"data.ingest":True}),"fields":json.dumps(["uuid"])})
+    r.raise_for_status()
+    o = r.json()
 
+    for rs in o["items"]:
+        print rs["uuid"]
 
 if __name__ == '__main__':
     main()

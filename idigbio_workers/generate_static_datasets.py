24241b479581ce69f2bce7c3d464119b2da072c1
Alex Thompson <godfoder@acis.ufl.edu>
2015-07-21 10:40:47 -0400

    working on static downloads from new system.

---
+++ idigbio_workers/generate_static_datasets.py
@@ -0,0 +1,71 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+
+import requests
+import json
+
+from idigbio_workers.lib.download import generate_files, get_recordsets, es, indexName
+from idigbio_workers.lib.query_shim import queryFromShim
+from idb.helpers.storage import IDigBioStorage
+#from idb.lib.data.export.eml import eml_from_recordset
+
+def runQuery(query):
+    return es.search(index=indexName,doc_type="record,mediarecords",body=query)
+
+def upload_download_file_to_ceph(s, dsname):
+    fkey = s.upload_file(dsname,"idigbio-static-downloads",dsname)
+    fkey.set_metadata('Content-Type', 'application/zip')
+    os.unlink(dsname)
+    return "http://s.idigbio.org/idigbio-static-downloads/" + dsname
+
+def upload_eml_file_to_ceph(s, tid, eml):
+    fkey = s.get_key(tid + ".eml","idigbio-static-downloads")
+    fkey.set_contents_from_string(eml)
+    fkey.set_metadata('Content-Type', 'application/xml')
+    fkey.make_public()
+    return "http://s.idigbio.org/idigbio-static-downloads/" + tid + ".eml"
+
+
+def main():
+    s = IDigBioStorage()
+    static_queries = [
+        ({},"idigbio"),
+        ({"hasImage": True},"idigbio-images"),
+    ]
+    rsquery = {
+        "query": {
+            "match_all": {}
+        },
+        "size": 0,
+        "aggs": {
+            "recordset_counts": {
+                "terms": {
+                    "field": "recordset",
+                    "size": 10000
+                }
+            }
+        }
+    }
+    ro = runQuery(rsquery)
+    if ro is not None:
+        for b in ro["aggregations"]["recordset_counts"]["buckets"]:
+            #print b["key"], b["doc_count"], b["doc_count"] * 7 / 10000            
+            static_queries.append(({
+                "recordset": b["key"]
+            },b["key"]))
+
+    count = 0
+    for q in reversed(static_queries):
+        print count, q
+        file_name, _, _ = generate_files(record_query=queryFromShim(q[0])["query"],form="dwca-csv",filename=q[1])
+        print q[1], file_name
+        u = upload_download_file_to_ceph(s,file_name)
+        # # rseml = eml_from_recordset(q[1],env="prod")
+        # # e = upload_eml_file_to_ceph(s,q[1],rseml)
+        print q[1], u
+        count += 1
+
+if __name__ == '__main__':
+    main()
---
+++ idigbio_workers/generate_static_datasets_index.py
@@ -0,0 +1,150 @@
+import os
+import sys
+import uuid
+import csv
+import datetime
+import dateutil.parser
+import requests
+import json
+import re
+from cStringIO import StringIO
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+
+from idb.lib.storage import IDigBioStorage
+
+def is_uuid(s):
+    try:
+        uuid.UUID(s)
+        return True
+    except:
+        return False
+
+ic_replacement_table = {}
+ic_extract = re.compile("^.*\(([a-zA-Z]+)\)$")
+
+def get_true_ic(v):    
+    m = ic_extract.match(v)
+    if m is not None:
+      v = m.groups()[0]
+    v = v.lower().strip()
+    if v in ic_replacement_table:
+      v = ic_replacement_table[v]
+    return v
+
+def main():
+    index_file_name = "index.txt"
+
+    query = {
+        "size": 0,
+        "aggs": {
+            "rs": {
+                "terms": {
+                    "field": "recordset",
+                    "size": 1000
+                },
+                "aggs": {
+                    "ic":{
+                        "terms": {
+                            "field": "institutioncode",
+                            "size": 1000,
+                        },
+                        "aggs": {
+                            "cc": {
+                                "terms":{
+                                    "field": "collectioncode",
+                                    "size": 1000,
+                                }
+                            }
+                        }
+                    }
+                }                
+            }            
+        }
+    }
+    r = requests.post("http://search.idigbio.org/idigbio/records/_search",data=json.dumps(query),headers={"Content-Type":"application/json"})
+    r.raise_for_status()
+    ro = r.json()
+
+    recordsets = {}
+    for rs_b in ro["aggregations"]["rs"]["buckets"]:
+        rsid = rs_b["key"]
+        ic = ""
+        cc = ""
+        if len(rs_b["ic"]["buckets"]) == 0:
+            ic = ""
+            cc = ""
+        elif len(rs_b["ic"]["buckets"]) == 1 or (
+                float(rs_b["ic"]["buckets"][0]["doc_count"]) / float(rs_b["doc_count"]) > 0.9
+            ):
+            ic_b = rs_b["ic"]["buckets"][0]
+            ic = get_true_ic(ic_b["key"])
+            if len(ic_b["cc"]["buckets"]) == 0:
+                cc = ""
+            elif len(ic_b["cc"]["buckets"]) == 1:
+                cc = ic_b["cc"]["buckets"][0]["key"]
+            else:
+                cc = "MULTIPLE"
+        else:
+            # print rs_b
+            ic = "MULTIPLE"
+            cc = "MULTIPLE"
+        recordsets[rsid] = {
+            "institutioncode": ic,
+            "collectioncode": cc
+        }
+
+    s = IDigBioStorage()
+    b = s.get_bucket("idigbio-static-downloads")
+
+    headers = ["zipfile","emlfile","etag","modified","recordset_id", "institutioncode", "collectioncode"]
+    files = {}
+
+    for k in b.list():
+        # Skip the index itself
+        if k == index_file_name:
+            continue
+
+        # Skip files older than 8 days
+        lm_d = dateutil.parser.parse(k.last_modified).date()        
+        if lm_d < (datetime.datetime.now() - datetime.timedelta(7)).date():
+            continue
+
+        fkey = k.name.split(".")[0]
+        if fkey not in files:
+            files[fkey] = {k:"" for k in headers}
+
+        if k.name.endswith(".eml"):
+            files[fkey]["emlfile"] = k.name
+        elif k.name.endswith(".zip"):
+            files[fkey]["zipfile"] = k.name
+            files[fkey]["modified"] = k.last_modified
+            files[fkey]["etag"] = k.etag
+            if is_uuid(fkey):
+                files[fkey]["recordset_id"] = fkey
+                if fkey in recordsets:
+                    files[fkey]["institutioncode"] = recordsets[fkey]["institutioncode"]
+                    files[fkey]["collectioncode"] = recordsets[fkey]["collectioncode"]
+                else:
+                    files[fkey]["institutioncode"] = ""
+                    files[fkey]["collectioncode"] = ""
+    
+    fil = StringIO()
+    
+    cw = csv.writer(fil,delimiter="\t")
+
+    cw.writerow(headers)
+    for k in files:
+        if files[k]["zipfile"] != "":
+            cw.writerow([files[k][h].replace("\"","") for h in headers])
+
+    fil.seek(0)
+
+    ik = b.get_key(index_file_name,validate=False)
+    ik.content_type = 'text/tsv'
+    ik.set_contents_from_file(fil)
+    ik.make_public()
+
+
+if __name__ == '__main__':
+    main()

d81fb758071a84e0ab3868185878fefd7a87b2b9
Alex Thompson <godfoder@acis.ufl.edu>
2015-07-22 14:06:30 -0400

    working on generating media files.

---
+++ idigbio_ingestion/mediaing.py
@@ -0,0 +1,74 @@
+from gevent.pool import Pool
+
+from idb.postgres_backend.db import PostgresDB
+
+import magic
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+import traceback
+
+s = requests.Session()
+
+auth = HTTPBasicAuth(os.environ.get("IDB_UUID"),os.environ.get("IDB_APIKEY"))
+
+def get_media(tup):
+    url, t, fmt = tup
+    print url, t, fmt
+
+    try:
+        media_req = s.get(url)
+        media_req.raise_for_status()
+        
+        detected_fmt = magic.from_buffer(media_req.content, mime=True)
+        print t, detected_fmt, fmt
+        # if detected_fmt == fmt:
+        #     apiimg_req = s.post("http://media.idigbio.org/upload/" + t, data={"filereference": url}, files={'file': media_req.content }, auth=auth)
+        #     apiimg_req.raise_for_status()
+        # else:
+        #     pass
+
+        return True
+    except:
+        traceback.print_exc()
+        return False
+
+def main():
+    db = PostgresDB()
+
+    media_urls = set()
+
+    print "Get Media URLs"
+
+    cur = db._get_ss_cursor()
+    cur.execute("SELECT lookup_key FROM idb_object_keys")
+    for r in cur:
+        media_urls.add(r["lookup_key"])
+
+    mime_mapping = {
+        "image/jpeg": "images"
+    }
+
+    def media_url_iterator():
+        print "Start Iterator"
+        cur = db._get_ss_cursor()
+        cur.execute("SELECT COALESCE(data ->> 'ac:accessURI', data ->> 'ac:bestQualityAccessURI', data ->> 'dcterms:identifier') as url, COALESCE(data ->> 'dcterms:format', data ->> 'dc:format') as format FROM data WHERE COALESCE(data ->> 'ac:accessURI', data ->> 'ac:bestQualityAccessURI', data ->> 'dcterms:identifier') IS NOT NULL")
+        for r in cur:
+            url = r["url"].replace("&amp;","&").strip()
+            if url not in media_urls:
+                if r["format"] in mime_mapping:
+                    yield (r["url"], mime_mapping[r["format"]], r["format"])
+                else:
+                    print "Unknown Format", r["format"]
+
+    p = Pool()
+    for _ in p.imap_unordered(get_media,media_url_iterator()):
+        pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()

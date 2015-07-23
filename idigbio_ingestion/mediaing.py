ac87b30a315a6bf035cd95f8626ddee604d8838c
Alex Thompson <godfoder@acis.ufl.edu>
2015-07-23 13:30:08 -0400

    working on media.

--- idigbio_ingestion/mediaing.py
+++ idigbio_ingestion/mediaing.py
@@ -13,7 +13,39 @@ import traceback
 
 s = requests.Session()
 
-auth = HTTPBasicAuth(os.environ.get("IDB_UUID"),os.environ.get("IDB_APIKEY"))
+auth = HTTPBasicAuth(os.environ.get("IDB_UUID"), os.environ.get("IDB_APIKEY"))
+
+import sqlite3
+
+sqlite_conn = sqlite3.connect("media.db")
+sqlite_conn.isolation_level = None
+sqlite_cur = sqlite_conn.cursor()
+
+
+db = PostgresDB()
+
+# Set a mime type to none to explicitly ignore it
+mime_mapping = {
+    "image/jpeg": "images",
+    "text/html": None,
+    "image/dng": None,
+    "application/xml": None,
+}
+
+
+def create_schema():
+    sqlite_cur.execute("BEGIN")
+    sqlite_cur.execute("""CREATE TABLE IF NOT EXISTS media (
+        url text PRIMARY KEY,
+        type text,
+        mime text,
+        retrieved timestamp,
+        etag text,
+        detected_mime text
+    )
+    """)
+    sqlite_conn.commit()
+
 
 def get_media(tup):
     url, t, fmt = tup
@@ -21,7 +53,7 @@ def get_media(tup):
     try:
         media_req = s.get(url)
         media_req.raise_for_status()
-        
+
         detected_fmt = magic.from_buffer(media_req.content, mime=True)
         if detected_fmt == fmt:
             # apiimg_req = s.post("http://media.idigbio.org/upload/" + t, data={"filereference": url}, files={'file': media_req.content }, auth=auth)
@@ -35,9 +67,41 @@ def get_media(tup):
         traceback.print_exc()
         return False
 
-def main():
-    db = PostgresDB()
 
+def write_urls_to_db(media_urls):
+    print "Start Inserts"
+
+    inserted_urls = set()
+
+    inserts = 0
+    scanned = 0
+    total_inserts = 0
+    cur = db._get_ss_cursor()
+    cur.execute(
+        "SELECT COALESCE(data ->> 'ac:accessURI', data ->> 'ac:bestQualityAccessURI', data ->> 'dcterms:identifier') as url, COALESCE(data ->> 'dcterms:format', data ->> 'dc:format') as format FROM data WHERE COALESCE(data ->> 'ac:accessURI', data ->> 'ac:bestQualityAccessURI', data ->> 'dcterms:identifier') IS NOT NULL")
+    sqlite_cur.execute("BEGIN")
+    for r in cur:
+        scanned += 1
+        url = r["url"].replace("&amp;", "&").strip()
+        if url not in media_urls and url not in inserted_urls:
+            if r["format"] in mime_mapping:
+                if mime_mapping[r["format"]] is not None:
+                    sqlite_cur.execute("INSERT INTO media (url,type,mime) VALUES (?,?,?)", (r[
+                                       "url"], mime_mapping[r["format"]], r["format"]))
+                    inserts += 1
+                    inserted_urls.add(url)
+            elif r["format"] is None:
+                pass
+            else:
+                print "Unknown Format", r["format"]
+        if inserts >= 10000:
+            total_inserts += inserts
+            sqlite_conn.commit()
+            print total_inserts, scanned
+            inserts = 0
+
+
+def get_postgres_media_urls():
     media_urls = set()
 
     print "Get Media URLs"
@@ -47,32 +111,14 @@ def main():
     for r in cur:
         media_urls.add(r["lookup_key"])
 
-    # Set a mime type to none to explicitly ignore it
-    mime_mapping = {
-        "image/jpeg": "images",
-        "text/html": None,
-        "image/dng": None,
-        "application/xml": None,
-    }
+    return media_urls
 
-    def media_url_iterator():
-        print "Start Iterator"
-        cur = db._get_ss_cursor()
-        cur.execute("SELECT COALESCE(data ->> 'ac:accessURI', data ->> 'ac:bestQualityAccessURI', data ->> 'dcterms:identifier') as url, COALESCE(data ->> 'dcterms:format', data ->> 'dc:format') as format FROM data WHERE COALESCE(data ->> 'ac:accessURI', data ->> 'ac:bestQualityAccessURI', data ->> 'dcterms:identifier') IS NOT NULL")
-        for r in cur:
-            url = r["url"].replace("&amp;","&").strip()
-            if url not in media_urls:
-                if r["format"] in mime_mapping:
-                    if mime_mapping[r["format"]] is not None:
-                        yield (r["url"], mime_mapping[r["format"]], r["format"])
-                elif r["format"] is None:
-                    pass
-                else:
-                    print "Unknown Format", r["format"]
 
-    p = Pool()
-    for _ in p.imap(get_media,media_url_iterator()):
-        pass
+def main():
+    create_schema()
+    media_urls = get_postgres_media_urls()
+    write_urls_to_db(media_urls)
+
 
 if __name__ == '__main__':
     main()

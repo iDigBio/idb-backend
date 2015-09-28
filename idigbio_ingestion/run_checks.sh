71e2990f1ca233ba57906608f3d62a7a03e89578
Dan Stoner <danstoner@gmail.com>
2015-09-28 14:24:26 -0400

    minor change output for easier copy paste from terminal text

--- idigbio_ingestion/run_checks.sh
+++ idigbio_ingestion/run_checks.sh
@@ -7,8 +7,8 @@ export PYTHONPATH=$( dirname "${DIR}" )
 
 python $DIR/db_rsids.py | xargs -n1 -P `nproc` python $DIR/db_check.py $@
 
-echo "Generating summary.csv..."
+echo "Generating... summary.csv"
 python $DIR/ds_sum_counts.py /mnt/data/new_ingestion/ > summary.csv
 
-echo "Converting summary.csv to columnar human readable report summary.pretty.txt..."
+echo "Converting summary.csv to columnar human readable report... summary.pretty.txt"
 column -ts ',' summary.csv | sort > summary.pretty.txt

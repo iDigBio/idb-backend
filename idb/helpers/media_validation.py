a434f24e8c2ebb58c16dfeddfe3b11b2adb089d8
Alex Thompson <godfoder@acis.ufl.edu>
2015-11-02 14:25:06 -0500

    work on waveforms.

--- idb/helpers/media_validation.py
+++ idb/helpers/media_validation.py
@@ -1,12 +1,17 @@
 import magic
 
-format_validators = {
-    "model/mesh": lambda url, t, fmt, content: (url.endswith(".stl"), "model/mesh")
-}
-
 def default_format_validator(url, t, fmt, content):
     mime = magic.from_buffer(content, mime=True)
     return (fmt == mime, mime)
+
+def audio_mpeg3_validator(url, t, fmt, content):
+    _, mime = default_format_validator(url, t, fmt, content)
+    return (mime in ["audo/mpeg3", "audio/mpeg", "audio/mp3"], mime)
+
+format_validators = {
+    "model/mesh": lambda url, t, fmt, content: (url.endswith(".stl"), "model/mesh"),
+    "audio/mpeg3": audio_mpeg3_validator
+}
 
 def get_validator(m):
     if m in format_validators:

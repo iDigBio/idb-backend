5277fcdacf505e003d9feb163a436acca50340cf
Dan Stoner <danstoner@gmail.com>
2015-11-23 18:46:23 -0500

    commit changes already living in prod

--- idb/daily.sh
+++ idb/daily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PYTHONPATH=/root/idb-backend/ python /root/idb-backend/idigbio_ingestion/update_publisher_recordset.py > /dev/null
+PYTHONPATH=/root/idb-backend/ python /root/idb-backend/idigbio_ingestion/update_publisher_recordset.py &> /dev/null

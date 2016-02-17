e7c54d5a9a07d3f5add7a53a1ed7be25eeb3725f
Dan Stoner <danstoner@gmail.com>
2016-02-17 11:22:31 -0500

    fights pyquery

--- idigbio_ingestion/lib/eml.py
+++ idigbio_ingestion/lib/eml.py
@@ -53,7 +53,7 @@ def parseEml(id, emlText):
     if rights is not None:        
         rights_para = rights.find("para")
         if rights_para is not None:
-            rights_citetitle = rights_para.find("citetitle")
+            rights_citetitle = rights.find("citetitle")
             if rights_citetitle is not None:
                 rights_text = rights_citetitle.text
                 logger.debug("rights_text(citetitle): {0}".format(rights_text))

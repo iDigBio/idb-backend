776830c680e4ae23af90bc2aeb13ed9e971d551c
Nathan Bird <nbird@ufl.edu>
2016-03-14 10:23:00 -0400

    Fix connect args for apidbpool
    When converted from module static connection to pool object the prefix
    and suffix args weren't handled correctly.

--- idb/postgres_backend/__init__.py
+++ idb/postgres_backend/__init__.py
@@ -19,8 +19,8 @@ psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 
 pg_conf = copy.deepcopy(config["postgres"])
 
-prefix = config["postgres"].pop("db_prefix", "")
-suffix = config["postgres"].pop("db_suffix", "")
+prefix = pg_conf.pop("db_prefix", "")
+suffix = pg_conf.pop("db_suffix", "")
 
 pg_conf["database"] = prefix + "api" + suffix
 pg_conf["cursor_factory"] = DictCursor

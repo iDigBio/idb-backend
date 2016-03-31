ab896f32849f7ec4de41f87d28c19633ee9c70f8
Nathan Bird <nbird@acis.ufl.edu>
2016-03-31 16:33:56 -0400

    Simplify flask_authn test using pytest
    This is a simpler version of the same test that still succeeds in
    getting the check to return false, now because the DB is empty and not
    because it can't find the flask app.
    
    re #3

--- idb/tests/test_helpers_idb_flask_authn.py
+++ idb/tests/test_helpers_idb_flask_authn.py
@@ -1,17 +1,9 @@
-import unittest
+from __future__ import division, absolute_import
+from __future__ import print_function, unicode_literals
 
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
-
-from helpers.idb_flask_authn import *  # noqa
+from idb.helpers.idb_flask_authn import check_auth
 
 
-class TestCheckAuth(unittest.TestCase):
-    def test_check_no_perms(self):
-        assert check_auth("2135faef-e12c-4b98-b788-05930d0ca290","f71cca621ba47b2c5316143de38dc628") is False
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_check_no_perms(client):
+    assert check_auth("2135faef-e12c-4b98-b788-05930d0ca290",
+                      "f71cca621ba47b2c5316143de38dc628") is False

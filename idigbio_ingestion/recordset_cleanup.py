ce9895c9c935757f7772940d8c0dd281b093cb14
Alex Thompson <godfoder@acis.ufl.edu>
2016-04-12 16:35:09 -0400

    added a mass deletion function for all old recordset stats.

--- idigbio_ingestion/recordset_cleanup.py
+++ idigbio_ingestion/recordset_cleanup.py
@@ -1,8 +1,10 @@
 import os
 import sys
 
+from idb.postgres_backend import apidbpool
 from idb.stats_collector import es, indexName
 import datetime
+
 
 def post_delete_stats(rsid):
     summary = {                                             
@@ -19,10 +21,49 @@ def post_delete_stats(rsid):
             "deleted": True,
             "commited": True,
         }
-    es.index(index=indexName,doc_type="digest",body=summary)
+    es.index(index=indexName, doc_type="digest", body=summary)
+
+
+def post_all_deleted_rs():
+    deleted_query = {
+      "query": {
+        "filtered": {
+          "filter": {
+            "term": {
+              "deleted": True
+            }
+          }
+        }
+      },
+      "size": 10000
+    }
+
+    deleted_recordsets = set()
+
+    rsp = es.search(index=indexName, doc_type="digest", body=deleted_query)
+    for rs in rsp["hits"]["hits"]:
+        deleted_recordsets.add(rs["_source"]["recordset_id"])
+
+    print "{} recordsets already marked as deleted in stats.".format(len(deleted_recordsets))
+
+    count = 0
+    with apidbpool.cursor() as cursor:
+        cursor.execute("SELECT id FROM uuids WHERE type='recordset' and deleted=true")
+
+        for r in cursor:
+            if r["id"] not in deleted_recordsets:
+                count += 1
+                print "Deleting {}.".format(r["id"])
+                post_delete_stats(r["id"])
+
+    print "{} recordsets deleted from stats.".format(count)
+
 
 def main():
-    post_delete_stats(sys.argv[1])
+    if len(sys.argv) > 1:
+        post_delete_stats(sys.argv[1])
+    else:
+        post_all_deleted_rs()
 
 if __name__ == '__main__':
     main()

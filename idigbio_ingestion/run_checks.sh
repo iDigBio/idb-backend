c6c8780af8f6625a3132411e8339e545e5788034
Nathan Bird <nbird@acis.ufl.edu>
2016-04-20 12:48:23 -0400

    New script ds_suspects.py, part run_checks process
    This is intended to eventually replace `ds_sum_counts.py` as well as
    adding new functionality: it will also try and identify some suspect
    rows that warrant further examination (e.g. all records deleted).
    
    For now it only generates the `suspect.csv` file and internally
    generates `summary.csv` to compare with the version generated by the old
    script for acceptance testing.

--- idigbio_ingestion/run_checks.sh
+++ idigbio_ingestion/run_checks.sh
@@ -9,6 +9,8 @@ python $DIR/db_rsids.py | xargs -n1 -P `nproc` python $DIR/db_check.py $@
 
 echo "Generating... summary.csv"
 python $DIR/ds_sum_counts.py ./ > summary.csv
+python $DIR/ds_suspects.py ./ summary.csv suspects.csv
 
 echo "Converting summary.csv to columnar human readable report... summary.pretty.txt"
 column -ts ',' summary.csv | sort > summary.pretty.txt
+column -ts ',' suspects.csv | sort > suspects.pretty.txt

737790fbfd096cb8fe92f9b819975b3aeb017797
Nathan Bird <nbird@acis.ufl.edu>
2016-05-19 09:26:37 -0400

    Rollup db-check-summarize into ds-sum-counts
    All db-check-summarize was doing beyond ds-sum-counts was to columnize
    the output, but there's no reason that sum counts shouldn't do that.
    
    Move that all together under the one command

--- idigbio_ingestion/db_check.py
+++ idigbio_ingestion/db_check.py
@@ -572,29 +572,5 @@ def all(since=None, ingest=False):
     except KeyboardInterrupt:
         logger.debug("Got KeyboardInterrupt")
         raise
-    summarize()
-
-
-def summarize(base='./', sum_filename='summary.csv', susp_filename="suspects.csv"):
     from .ds_sum_counts import main as ds_sum_counts
-
-    logger.info("Generating... %s", sum_filename)
-    ds_sum_counts(base, sum_filename, susp_filename)
-    sum_pretty_filename = sum_filename.replace('.csv', '.pretty.txt')
-    susp_pretty_filename = susp_filename.replace('.csv', '.pretty.txt')
-    logger.info("Converting %s (all recordsets subject to ingestion) to columnar human readable report... %r",
-                sum_filename, sum_pretty_filename)
-    columnize(sum_filename, sum_pretty_filename)
-
-    logger.info("Converting %s (recordsets with questionable updates) to columnar human readable report... %s",
-                susp_filename, susp_pretty_filename)
-    columnize(susp_filename, susp_pretty_filename)
-
-
-def columnize(ifile, ofile):
-    #column -ts ',' summary.csv | sort > summary.pretty.txt
-    p = subprocess.Popen(['column', '-ts', ',', ifile], stdout=subprocess.PIPE)
-    lines = p.stdout.readlines()
-    with AtomicFile(ofile, 'w', encoding='utf-8') as out:
-        for l in lines:
-            out.write(l)
+    ds_sum_counts('./', sum_filename='summary.csv', susp_filename="suspects.csv")

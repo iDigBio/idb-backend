dc7e9758398d5c2fb28bdd4bd3180e1f3262b821
Nathan Bird <nbird@acis.ufl.edu>
2016-05-20 09:34:23 -0400

    pep8 reformatting

--- idb/data_api/v2.py
+++ idb/data_api/v2.py
@@ -7,13 +7,16 @@ from idb.helpers.idb_flask_authn import requires_auth
 
 from idb.helpers.cors import crossdomain
 
-this_version = Blueprint(__name__,__name__)
+this_version = Blueprint(__name__, __name__)
 
-def format_list_item(t,uuid,etag,modified,version,parent):
+
+def format_list_item(t, uuid, etag, modified, version, parent):
     links = {}
     if t in current_app.config["PARENT_MAP"] and parent is not None:
-        links[current_app.config["PARENT_MAP"][t]] = url_for(".item",t=current_app.config["PARENT_MAP"][t],u=parent,_external=True)
-    links[t] = url_for(".item",t=t,u=uuid,_external=True)
+        links[current_app.config["PARENT_MAP"][t]] = url_for(
+            ".item",
+            t=current_app.config["PARENT_MAP"][t], u=parent, _external=True)
+    links[t] = url_for(".item", t=t, u=uuid, _external=True)
 
     return {
         "type": t,
@@ -24,8 +27,9 @@ def format_list_item(t,uuid,etag,modified,version,parent):
         "links": links,
     }
 
-def format_item(t,uuid,etag,modified,version,parent,data,siblings,ids):
-    r = format_list_item(t,uuid,etag,modified,version,parent)
+
+def format_item(t, uuid, etag, modified, version, parent, data, siblings, ids):
+    r = format_list_item(t, uuid, etag, modified, version, parent)
     del r["links"][t]
     for l in r["links"]:
         r["links"][l] = [r["links"][l]]
@@ -35,7 +39,7 @@ def format_item(t,uuid,etag,modified,version,parent,data,siblings,ids):
         for k in siblings:
             l[k + "s"] = []
             for i in siblings[k]:
-                l[k + "s"].append(url_for(".item",t=k,u=i,_external=True))
+                l[k + "s"].append(url_for(".item", t=k, u=i, _external=True))
 
     r["type"] = t
     r["data"] = data
@@ -43,10 +47,13 @@ def format_item(t,uuid,etag,modified,version,parent,data,siblings,ids):
     r["recordIds"] = ids
     return r
 
-@this_version.route('/view/<string:t>/<uuid:u>/<string:st>', methods=['GET','OPTIONS'])
+
+@this_version.route('/view/<string:t>/<uuid:u>/<string:st>',
+                    methods=['GET', 'OPTIONS'])
 @crossdomain(origin="*")
-def subitem(t,u,st):
-    if not (t in current_app.config["SUPPORTED_TYPES"] and st in current_app.config["SUPPORTED_TYPES"]):
+def subitem(t, u, st):
+    if not (t in current_app.config["SUPPORTED_TYPES"] and
+            st in current_app.config["SUPPORTED_TYPES"]):
         return json_error(404)
 
     limit = request.args.get("limit")
@@ -62,26 +69,28 @@ def subitem(t,u,st):
 
     r = {}
     l = [
-        format_list_item(
-            st,
-            v["uuid"],
-            v["etag"],
-            v["modified"],
-            v["version"],
-            v["parent"],
-        ) for v in idbmodel.get_children_list(str(u), "".join(st[:-1]),limit=limit,offset=offset)
+        format_list_item(st,
+                         v["uuid"],
+                         v["etag"],
+                         v["modified"],
+                         v["version"],
+                         v["parent"], )
+        for v in idbmodel.get_children_list(
+            str(u), "".join(st[:-1]),
+            limit=limit, offset=offset)
     ]
 
     r["items"] = l
     r["itemCount"] = idbmodel.get_children_count(str(u), "".join(st[:-1]))
     return jsonify(r)
 
-@this_version.route('/view/<string:t>/<uuid:u>', methods=['GET','OPTIONS'])
+
+@this_version.route('/view/<string:t>/<uuid:u>', methods=['GET', 'OPTIONS'])
 @crossdomain(origin="*")
-def item(t,u):
+def item(t, u):
     if t not in current_app.config["SUPPORTED_TYPES"]:
         return json_error(404)
-    
+
     version = request.args.get("version")
 
     v = idbmodel.get_item(str(u), version=version)
@@ -89,25 +98,18 @@ def item(t,u):
         if v["data"] is None:
             return json_error(500)
 
-        if v["type"] +"s" == t:
-            r = format_item(
-                t,
-                v["uuid"],
-                v["etag"],
-                v["modified"],
-                v["version"],
-                v["parent"],
-                v["data"],
-                v["siblings"],
-                v["recordids"]
-            )
+        if v["type"] + "s" == t:
+            r = format_item(t, v["uuid"], v["etag"], v["modified"],
+                            v["version"], v["parent"], v["data"],
+                            v["siblings"], v["recordids"])
             return jsonify(r)
         else:
             return json_error(404)
     else:
         return json_error(404)
 
-@this_version.route('/view/<uuid:u>', methods=['GET','OPTIONS'])
+
+@this_version.route('/view/<uuid:u>', methods=['GET', 'OPTIONS'])
 @crossdomain(origin="*")
 def item_no_type(u):
     version = request.args.get("version")
@@ -117,22 +119,15 @@ def item_no_type(u):
         if v["data"] is None:
             return json_error(500)
 
-        r = format_item(
-            v["type"] + "s",
-            v["uuid"],
-            v["etag"],
-            v["modified"],
-            v["version"],
-            v["parent"],
-            v["data"],
-            v["siblings"],
-            v["recordids"]
-        )
+        r = format_item(v["type"] + "s", v["uuid"], v["etag"], v["modified"],
+                        v["version"], v["parent"], v["data"], v["siblings"],
+                        v["recordids"])
         return jsonify(r)
     else:
         return json_error(404)
 
-@this_version.route('/view/<string:t>', methods=['GET','OPTIONS'])
+
+@this_version.route('/view/<string:t>', methods=['GET', 'OPTIONS'])
 @crossdomain(origin="*")
 def list(t):
     if t not in current_app.config["SUPPORTED_TYPES"]:
@@ -151,26 +146,27 @@ def list(t):
 
     r = {}
     l = [
-        format_list_item(
-            t,
-            v["uuid"],
-            v["etag"],
-            v["modified"],
-            v["version"],
-            v["parent"],
-        ) for v in idbmodel.get_type_list("".join(t[:-1]), limit=limit, offset=offset)
+        format_list_item(t,
+                         v["uuid"],
+                         v["etag"],
+                         v["modified"],
+                         v["version"],
+                         v["parent"], )
+        for v in idbmodel.get_type_list("".join(
+            t[:-1]), limit=limit, offset=offset)
     ]
 
     r["items"] = l
     r["itemCount"] = idbmodel.get_type_count("".join(t[:-1]))
     return jsonify(r)
 
-@this_version.route('/view', methods=['GET','OPTIONS'])
+
+@this_version.route('/view', methods=['GET', 'OPTIONS'])
 @crossdomain(origin="*")
 def view():
     r = {}
     for t in current_app.config["SUPPORTED_TYPES"]:
-        r[t] = url_for(".list",t=t,_external=True)
+        r[t] = url_for(".list", t=t, _external=True)
     return jsonify(r)
 
 # Index will be handled at root for component assembly

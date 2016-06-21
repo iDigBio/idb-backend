cbf32a9f1a514d31ad20d69c47caf85eaffcc3f3
Nathan Bird <nbird@acis.ufl.edu>
2016-06-21 16:54:59 -0400

    update the module's globals in config.update_environment
    Otherwise environment data specified in a config file referenced with
    `--config=...` gets silently skipped

--- idb/config/__init__.py
+++ idb/config/__init__.py
@@ -8,18 +8,19 @@ conf_paths = ["/etc/idigbio/", "~/", "."]
 
 config = {}
 
-def update_environment(config):
-    env = config.get('env')
+def update_environment(env):
     if not env:
         return
     os.environ.update(env)
+    for k,v in env.items():
+        globals()[k] = v
 
 def load_config_file(p):
     with open(p, "rb") as conf:
         logger.debug("Reading config from %r", p)
         json_config = json.load(conf)
         config.update(json_config)
-        update_environment(json_config)
+        update_environment(json_config.get('env'))
 
 for p in conf_paths:
     fp = os.path.join(os.path.abspath(os.path.expanduser(p)), "idigbio.json")

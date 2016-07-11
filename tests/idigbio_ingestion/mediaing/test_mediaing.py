a2a50ee73e0b615883ddbfa238f83d14e962df90
Nathan Bird <nbird@acis.ufl.edu>
2016-07-11 11:12:11 -0400

    Fix test to follow function move
    the `check_ignore_media` function moved to be more closely associated
    with the only place that's using it. This test needed to be updated to
    refer to the new location.

--- tests/idigbio_ingestion/mediaing/test_mediaing.py
+++ tests/idigbio_ingestion/mediaing/test_mediaing.py
@@ -8,9 +8,10 @@ here. At the minimum this imports that module: syntax check.
 import pytest
 
 from idigbio_ingestion import mediaing
+from idigbio_ingestion.mediaing import updatedb
 
 
 def test_check_ignore_media():
     ip = mediaing.IGNORE_PREFIXES[0]
-    assert mediaing.check_ignore_media(ip + "asdf")
-    assert not mediaing.check_ignore_media("asdfasdf")
+    assert updatedb.check_ignore_media(ip + "asdf")
+    assert not updatedb.check_ignore_media("asdfasdf")

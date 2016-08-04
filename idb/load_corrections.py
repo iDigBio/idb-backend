0abee05fc1ce08d2d1e43c0d96fee3c196cfb956
Alex Thompson <godfoder@acis.ufl.edu>
2016-08-04 15:33:06 -0400

    iterative loading for extended taxon.

--- idb/load_corrections.py
+++ idb/load_corrections.py
@@ -14,6 +14,9 @@ def main():
     # for s in taxon.get_sources():
     #     c.clear_source(s)
 
+    for s in taxon_extended.get_sources():
+        c.clear_source(s)
+
     with CorrectionsLoader() as loader:
         # for dr in locality.get_data():
         #     loader.add_corrections(dr[0], dr[1], dr[2], approved=dr[3])
@@ -21,8 +24,7 @@ def main():
         # for dr in taxon.get_data(os.path.expanduser("~/taxon/checklist1.zip")):
         #     loader.add_corrections(dr[0], dr[1], dr[2], approved=dr[3])
 
-        for dr in taxon_extended.get_data():
-            loader.add_corrections(*dr[0:3], approved=dr[3])
+        loader.add_corrections_iter(taxon_extended.get_data())
 
 if __name__ == '__main__':
     main()

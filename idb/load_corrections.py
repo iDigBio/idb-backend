503d82a4ff4067613069210c651ec98b996836d7
Alex Thompson <godfoder@acis.ufl.edu>
2016-08-29 16:45:34 -0400

    re-enable all load corrections.

--- idb/load_corrections.py
+++ idb/load_corrections.py
@@ -8,23 +8,23 @@ from data_tables import locality, taxon, taxon_extended
 def main():
     c = CorrectionsLoader()
 
-    # for s in locality.get_sources():
-    #     c.clear_source(s)
+    for s in locality.get_sources():
+        c.clear_source(s)
 
     for s in taxon.get_sources():
         c.clear_source(s)
 
-    # for s in taxon_extended.get_sources():
-    #     c.clear_source(s)
+    for s in taxon_extended.get_sources():
+        c.clear_source(s)
 
     with CorrectionsLoader() as loader:
-        # for dr in locality.get_data():
-        #     loader.add_corrections(dr[0], dr[1], dr[2], approved=dr[3])
+        for dr in locality.get_data():
+            loader.add_corrections(dr[0], dr[1], dr[2], approved=dr[3])
 
         for dr in taxon.get_data(os.path.expanduser("~/Downloads/backbone-current-sorted.zip")):
             loader.add_corrections(dr[0], dr[1], dr[2], approved=dr[3])
 
-        #loader.add_corrections_iter(taxon_extended.get_data())
+        loader.add_corrections_iter(taxon_extended.get_data())
 
 if __name__ == '__main__':
     main()

73f218eadf710055399c5969c67e4a4e151d5c2d
Nathan Bird <nbird@acis.ufl.edu>
2016-09-07 16:05:37 -0400

    Get redis conn based on ENV; make it a memoized func
    Previously it always pointed at the URL of the production redis DB. Now
    it points at the same result DB configured for celery. This fixes a
    potential bug where similar download request between beta and prod has
    the result in one location but not in the other, but the code attempts
    to fetch it in both places anyways.

--- idigbio_workers/__init__.py
+++ idigbio_workers/__init__.py
@@ -2,8 +2,18 @@ import os
 import sys
 from celery import Celery
 
+from idb.helpers.memoize import memoized
+
 app = Celery('tasks')
 env = os.getenv("ENV", "prod")
 app.config_from_object('idigbio_workers.config.' + env)
 
 from tasks.download import downloader, send_download_email  # noqa
+
+@memoized()
+def get_redis_conn():
+    import redis
+    from kombu.utils.url import _parse_url
+    url = app.conf['CELERY_RESULT_BACKEND']
+    scheme, host, port, user, password, path, query = _parse_url(url)
+    return redis.StrictRedis(host=host, port=port, db=path)

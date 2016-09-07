855ffbf3535b017e52598e08bde9ecb8758e077d
Nathan Bird <nbird@acis.ufl.edu>
2016-09-07 09:57:03 -0400

    Adding BROKER_TRANSPORT_OPTIONS to beta redis config
    This is based on the recommendations at
    http://docs.celeryproject.org/en/latest/getting-started/brokers/redis.html
    
    Since the docs say that when enabling this new and old celery workers
    can't talk to eachother and I'm in the process of trying to rollout a
    new generation of celery workers I've bumped the redis DB to `/1` so
    that there isn't any task confusion between the new and old workers

--- idigbio_workers/config/beta.py
+++ idigbio_workers/config/beta.py
@@ -1,5 +1,10 @@
-BROKER_URL = 'redis://idb-redis-celery-beta.acis.ufl.edu:6379/0'
-CELERY_RESULT_BACKEND = 'redis://idb-redis-celery-beta.acis.ufl.edu:6379/0'
+BROKER_URL = 'redis://idb-redis-celery-beta.acis.ufl.edu:6379/1'
+BROKER_TRANSPORT_OPTIONS = {
+    'fanout_prefix': True,
+    'fanout_patterns': True,
+    'visibility_timeout': 43200
+}
+CELERY_RESULT_BACKEND = 'redis://idb-redis-celery-beta.acis.ufl.edu:6379/1'
 
 CELERYD_CONCURRENCY = 4
 CELERYD_PREFETCH_MULTIPLIER = 1

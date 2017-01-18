4edc52122fd8e164251d60b82a1ec6a5953e5874
Nathan Bird <nbird@acis.ufl.edu>
2017-01-18 15:41:54 -0500

    Celery 4 introduction
    This is the start of work on celery 4 which has a bunch of changes and
    improvements, and isn't compatible with our current celery release
    infrastructure: we need to upgrade all our workers and api servers
    together; hence the major version bump.

--- idigbio_workers/config/beta.py
+++ idigbio_workers/config/beta.py
@@ -1,16 +1,15 @@
-BROKER_URL = 'redis://idb-redis-celery-beta.acis.ufl.edu:6379/1'
-BROKER_TRANSPORT_OPTIONS = {
+broker_url = 'redis://idb-redis-celery-beta.acis.ufl.edu:6379/1'
+broker_transport_options = {
     'fanout_prefix': True,
     'fanout_patterns': True,
     'visibility_timeout': 43200
 }
-CELERY_RESULT_BACKEND = 'redis://idb-redis-celery-beta.acis.ufl.edu:6379/1'
+result_backend = 'redis://idb-redis-celery-beta.acis.ufl.edu:6379/1'
 
-CELERYD_CONCURRENCY = 4
-CELERYD_PREFETCH_MULTIPLIER = 1
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TIMEZONE = 'America/New_York'
-CELERY_ENABLE_UTC = True
-CELERY_DISABLE_RATE_LIMITS = True
+worker_concurrency = 4
+worker_prefetch_multiplier = 1
+
+
+timezone = 'America/New_York'
+
+worker_disable_rate_limits = True

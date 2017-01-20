a34039fbead618d7563833fbe2d3e468e5282189
Alex Thompson <godfoder@acis.ufl.edu>
2017-01-20 13:42:43 -0500

    Add consensus checking to correction loading for extended taxon correction.

--- idb/data_tables/taxon_extended.py
+++ idb/data_tables/taxon_extended.py
@@ -1,29 +1,39 @@
 import json
+from collections import Counter
+from idb.helpers.etags import objectHasher
 
 def get_data():
+
+    keys = {}
+    values = {}
     with open("taxon_kv.txt") as tkvf:
         for l in tkvf:
-            # There could be 2 or more elements of o, so we cant to a simple assignment
+            # There could be 2 or more elements of o, so we cant do a simple assignment
             o = json.loads(l)
             k = o[0]
             v = o[1]
-            # correction_value = {
-            #     "gbif:canonicalName": v["dwc:scientifcName"],
-            #     "dwc:taxonomicStatus": v["dwc:taxonomicStatus"],
-            #     "dwc:taxonID": v["dwc:taxonID"],
-            #     "dwc:taxonRank": v["dwc:taxonRank"],
-            #     "dwc:nameAccordingToID": "d7dddbf4-2cf0-4f39-9b2a-bb099caae36c",
-            #     "dwc:nameAccordingTo": "GBIF Backbone Taxonomy",
-            #     "gbif:VernacularName": v.get("gbif:VernacularName")
-            # }
-            if "dwc:scientificName" in v:
-                v["gbif:canonicalName"] = v["dwc:scientificName"]
-                del v["dwc:scientificName"]
 
-            if "flag_taxon_match_failed" not in v:
-                v["flag_gbif_taxon_corrected"] = True
+            k_etag = objectHasher("sha256", k, sort_arrays=True)
+            if k_etag not in keys:
+                keys[k_etag] = (k, Counter())
 
-            yield (k, v, 'gbif_checklist_extended', True)
+            v_etag = objectHasher("sha256", v, sort_arrays=True)
+            if v_etag not in values:
+                values[v_etag] = v
+
+            keys[k_etag][1][v_etag] += 1
+
+    for k_etag, (k, value_counts) in keys.items():
+        v = values[value_counts.most_common(1)[0][0]]
+
+        if "dwc:scientificName" in v:
+            v["gbif:canonicalName"] = v["dwc:scientificName"]
+            del v["dwc:scientificName"]
+
+        if "flag_taxon_match_failed" not in v:
+            v["flag_gbif_taxon_corrected"] = True
+
+        yield (k, v, 'gbif_checklist_extended', True)
 
 def get_sources():
     return ["gbif_checklist_extended"]

1ad37d96e992536c1cd05ad44979f3dacbed9e79
Dan Stoner <danstoner@gmail.com>
2017-03-17 09:47:09 -0400

    parameterize script inputs

--- scripts/fix-one-acl-public.py
+++ scripts/fix-one-acl-public.py
@@ -1,11 +1,21 @@
 # coding: utf-8
 from __future__ import absolute_import
 
+import argparse
 import boto3
 from idb import config
 
-bucket = 'idigbio-images-prod'
-etag = 'fc778eb00cae12784a5328e5ac1df2e1'
+
+argparser = argparse.ArgumentParser(description='Script to mark an s3 object as public (basically world readable).')
+argparser.add_argument("-b", "--bucket", required=True, help="Name of s3 bucket. Example: idigbio-images-prod")
+argparser.add_argument("-e", "--etag", required=True, help="etag of the object")
+args = argparser.parse_args()
+
+bucket = args.bucket
+etag = args.etag
+
+#bucket = 'idigbio-images-prod'
+#etag = 'fc778eb00cae12784a5328e5ac1df2e1'
 
 PUBLIC_READ = {u'Grantee': {u'Type': 'Group',
                             u'URI': 'http://acs.amazonaws.com/groups/global/AllUsers'},

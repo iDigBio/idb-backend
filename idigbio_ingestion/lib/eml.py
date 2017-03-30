fbd258cb2eb7bbaaa87ba40450a93f00c958a926
Dan Stoner <danstoner@gmail.com>
2017-03-30 11:09:03 -0400

    modify log line wording to make grep for "license" possible

--- idigbio_ingestion/lib/eml.py
+++ idigbio_ingestion/lib/eml.py
@@ -67,10 +67,10 @@ def parseEml(id, emlText):
             logger.debug('Unmatched data license "' + rights_text + '" in ' + id)
             collection["data_rights"] = "Unknown License, assume Public Domain"
         else:
-            logger.debug('Matched "' + rights_text + '" in ' + id + ' to "' + acceptable_licenses_trans[rights_text] + '"')
+            logger.debug('Matched data license "' + rights_text + '" in ' + id + ' to "' + acceptable_licenses_trans[rights_text] + '"')
             collection["data_rights"] = acceptable_licenses_trans[rights_text]
     else:
-        logger.debug('No rights text found in intellectualRights, using "No license, assume Public Domain" for ' + id)
+        logger.debug('No data license text found in intellectualRights, using "No license, assume Public Domain" for ' + id)
         collection["data_rights"] = "No license, assume Public Domain"
 
     collection["contacts"] = []

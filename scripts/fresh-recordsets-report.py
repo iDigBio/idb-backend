abc9a6b5f4c1ae80b818e446ad4a705704c62b2c
Dan Stoner <danstoner@gmail.com>
2018-10-03 14:05:53 -0400

    add age and default

--- scripts/fresh-recordsets-report.py
+++ scripts/fresh-recordsets-report.py
@@ -1,11 +1,18 @@
 from __future__ import division, absolute_import
 from __future__ import print_function
 
+import json
 
 from idb import config
 
 from idb.postgres_backend.db import PostgresDB
 
+def output_pretty(mylist):
+    print("(Will become pretty printed...)")
+    print(mylist)
+
+def output_json(mylist):
+    print(json.dumps(mylist))
 
 def main():
     import argparse
@@ -19,12 +26,28 @@ def main():
                              help='Output format (default: pretty)', default='pretty',
                              choices=['pretty','json'])  # for now only support pretty (plain text) or JSON
 
+    parser.add_argument('-a', '--age',
+                             help='Number of days ago from today to mark beginning of "recent" period.', default=30, type=int)
+
 
     args = parser.parse_args()
 
-    #db = PostgresDB()
 
-    print (args)
+    db = PostgresDB()
+
+    sample_list = [{"key":"value"}]
+
+
+
+
+
+
+
+    if args.output_format == "pretty":
+        output_pretty(sample_list)
+
+    if args.output_format == "json":
+        output_json(sample_list)
 
 if __name__ == "__main__":
     main()

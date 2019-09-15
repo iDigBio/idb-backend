cc0aa7b1601de5b4cd8557595ef9ed901d2190e1
Dan Stoner <danstoner@gmail.com>
2019-09-15 14:12:52 -0400

    remove the commented-out code for vertnet and github issues (#54)

--- scripts/data-ingestion-status-report.py
+++ scripts/data-ingestion-status-report.py
@@ -7,26 +7,8 @@ import datetime
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
-### Run with:
-### $ GITHUB_ACCESS_TOKEN=*your_github_access_token* python data_ingestion_status_report.py
-
-since_date = None
 parser = argparse.ArgumentParser(description='generate monthly Data Ingestion status report')
-parser.add_argument('--since', dest='since_date', required=False, help='The "since" date for github issues reporting in the form of YYYY-MM-DD')
 args = parser.parse_args()
-
-if since_date is not None:
-    # verify here that the supplied date argument is an actual date
-    try:
-        since_date = datetime.datetime.strftime(datetime.datetime.strptime(args.since_date, '%Y-%m-%d'),'%Y-%m-%d')
-    except:
-        print '''
-*****************************************************************
-ERROR:  since_date must be supplied in the format --> YYYY-MM-DD
-*****************************************************************
-'''
-        raise SystemExit
-
 
 buff = ''
 
@@ -78,37 +60,6 @@ These data were incorporated into iDigBio by the standard Data Ingestion process
 
 buff = buff + hr + "\n"
 
-# Generate VertNet section
-
-# buff = buff + "VertNet Report since " + since_date + ":\n\n"
-#
-# buff = buff + "(github login and access permission are required to view issues)\n\n"
-# if os.environ.get('GITHUB_ACCESS_TOKEN') is None:
-#     buff = buff + "*Unable to access VertNet github issues to generate report. Check environment variables.* \n\n"
-# else:
-#     access_token = os.environ.get('GITHUB_ACCESS_TOKEN')
-#     api_priv_url = 'https://api.github.com/repos/VertNet/Tasks/issues'
-#     q_month = '?since=' + since_date
-#
-#     r = requests.get(api_priv_url + q_month, headers={'Authorization': 'token ' + access_token})
-#
-#     buff = buff + "NEW issues:\n"
-#     for each in r.json():
-#         if "title" in each:
-#             for labels in each["labels"]:
-#                 if "name" in labels:
-#                     if labels["name"] == "new":
-#                         buff = buff + each["html_url"] + "  " + each["title"] + "\n"
-#
-#     ## leave UPDATED out of report for now.
-#     #buff = buff + "\nUPDATED issues:\n"
-#     #for each in r.json():
-#     #    if "title" in each:
-#     #        buff = buff + each["html_url"] + "  " + each["title"] + "\n"
-
-    
-#buff = buff + "\n" + hr
-
 buff = buff + '''
 Notes:
 
@@ -139,4 +90,3 @@ print "***** BEGIN *****"
 print buff
 
 print "***** END *****"
-

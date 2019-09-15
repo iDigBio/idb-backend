97fd23e9f12778e172f87a0644c3f9bb012f71f7
Dan Stoner <danstoner@gmail.com>
2019-09-15 16:51:49 -0400

    add a count of updated recordsets, switch to print function

--- scripts/data-ingestion-status-report.py
+++ scripts/data-ingestion-status-report.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import idigbio
 import locale
 import requests
@@ -28,6 +29,21 @@ record_count = locale.format("%d", api.count_records(), grouping=True)
 media_record_count = locale.format("%d", api.count_media(), grouping=True)
 recordset_count = locale.format("%d", api.count_recordsets(), grouping=True)
 
+# Updated recordsets is an approximation based on the number of items
+# appearing in the Ingestion summary log, minus 15% to take into account
+# that we usually overlap ingest periods for db-check operations by a few weeks.
+# Currently the summary is in an expected, hard-coded path:
+#   /mnt/data/new_ingestion/summary.pretty.txt
+summary_path = "/mnt/data/new_ingestion/summary.pretty.txt"
+if os.path.exists(summary_path):
+    linecount = len(open(summary_path).readlines())
+    updated_recordsets_blurb = ("Approximately {0}".format(int(linecount * .85)) +
+        " recordsets were re-published by data providers with updated data.")
+else:
+# If we cannot find the summary file, use a generic madlibs line
+    updated_recordsets_blurb = ("Approximately ___ " +
+        "recordsets were re-published by data providers with updated data.")
+
 buff = buff + 'COUNTS\n\n'
 buff = buff + 'Recordsets: ' + str(recordset_count) + "\n"
 buff = buff + 'Records: ' + record_count + "\n"
@@ -50,11 +66,10 @@ buff = buff + hr + "\n"
 buff = buff + '''
 UPDATED Recordsets since last report:
 
+'''
 
-Approximately ___ recordsets were re-published by data providers with updated data.
-
+buff = buff + updated_recordsets_blurb + "\n" + '''
 These data were incorporated into iDigBio by the standard Data Ingestion process.
-
 
 '''
 
@@ -85,8 +100,8 @@ https://github.com/vertnet/tasks
 buff = buff + hr
 
 
-print "***** BEGIN *****"
+print ("***** BEGIN *****")
 
-print buff
+print (buff)
 
-print "***** END *****"
+print ("***** END *****")

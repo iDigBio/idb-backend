cfe9b67a053853d098f77a2c4517c2d237fc8118
Dan Stoner <danstoner@gmail.com>
2019-09-15 20:08:40 -0400

    wip

--- scripts/data-ingestion-status-report.py
+++ scripts/data-ingestion-status-report.py
@@ -1,10 +1,12 @@
-from __future__ import print_function
+from __future__ import print_function, absolute_import
 import idigbio
 import locale
 import requests
 import os
 import argparse
 import datetime
+from idb.postgres_backend.db import PostgresDB
+from idb.config import config
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
@@ -28,6 +30,17 @@ api = idigbio.json()
 record_count = locale.format("%d", api.count_records(), grouping=True)
 media_record_count = locale.format("%d", api.count_media(), grouping=True)
 recordset_count = locale.format("%d", api.count_recordsets(), grouping=True)
+
+# Paused recordsets is the count of recordsets where ingest is true,
+# and paused is also true.  Will not count paused recordsets where
+# ingest is false.
+db = PostgresDB()
+sql = """
+      SELECT count(*) FROM recordsets WHERE ingest = true AND ingest_is_paused = true;
+"""
+paused_count = db.execute(sql)
+
+print("PAUSED COUNT... '{0}'".format(paused_count))
 
 # Updated recordsets is an approximation based on the number of items
 # appearing in the Ingestion summary log, minus 15% to take into account

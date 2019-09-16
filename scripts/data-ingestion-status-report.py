f8a083acd2a9424888709f91c3f444c4281ecb68
Dan Stoner <danstoner@gmail.com>
2019-09-15 21:01:59 -0400

    Add count of paused recordsets to report (#55)
    * add PAUSED count, fixup some output line spacing

--- scripts/data-ingestion-status-report.py
+++ scripts/data-ingestion-status-report.py
@@ -1,10 +1,12 @@
-from __future__ import print_function
+from __future__ import print_function, absolute_import
 import idigbio
 import locale
 import requests
 import os
 import argparse
 import datetime
+from idb.postgres_backend.db import PostgresDB
+from idb.config import config
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
@@ -28,6 +30,16 @@ api = idigbio.json()
 record_count = locale.format("%d", api.count_records(), grouping=True)
 media_record_count = locale.format("%d", api.count_media(), grouping=True)
 recordset_count = locale.format("%d", api.count_recordsets(), grouping=True)
+
+# Paused count is the count of recordsets where ingest is true,
+# and paused is also true.  Will not count paused recordsets where
+# ingest is false.
+db = PostgresDB()
+sql = """
+      SELECT count(*) FROM recordsets WHERE ingest = true AND ingest_is_paused = true;
+"""
+db_r = db.fetchone(sql)
+paused_count = db_r["count"]
 
 # Updated recordsets is an approximation based on the number of items
 # appearing in the Ingestion summary log, minus 15% to take into account
@@ -60,8 +72,7 @@ NEW Publishers and/or Recordsets added to iDigBio since last report:
 
 '''
 
-buff = buff + hr + "\n"
-
+buff = buff + hr
 
 buff = buff + '''
 UPDATED Recordsets since last report:
@@ -73,7 +84,9 @@ These data were incorporated into iDigBio by the standard Data Ingestion process
 
 '''
 
-buff = buff + hr + "\n"
+buff = buff + "There are currently {0} PAUSED recordsets.".format(paused_count) + "\n\n"
+
+buff = buff + hr
 
 buff = buff + '''
 Notes:

a4f87fdbe168ddf6b5cb169da3543bb444e5a3cc
Ronald Canepa <rcanepa@ufl.edu>
2020-07-10 16:34:46 -0400

    added temporary trace logging to file to try to understand process better. also reduced verbosity of systemd call

--- idb/stats/collect.py
+++ idb/stats/collect.py
@@ -100,17 +100,16 @@ def collect_stats(collect_datetime, es=None):
     date_max = collect_datetime.date()
 
     # Trap duplicate runs and abort.
-    c = es.count(index="stats", doc_type="search", body={"query": {
-        "term": {
-            "harvest_date": date_max.isoformat()
-        }
-    }})
-    if c["count"] != 0:
-        logger.warn("Duplicate run detected, aborting")
-        return
+    # c = es.count(index="stats", doc_type="search", body={"query": {
+    #     "term": {
+    #         "harvest_date": date_max.isoformat()
+    #     }
+    # }})
+    # if c["count"] != 0:
+    #     logger.warn("Duplicate run detected, aborting")
+    #     return
 
     logger.info("Collecting stats for %s to %s", date_min, date_max)
-
     recordset_stats = defaultdict(new_stats_dict)
 
     # print date_min, date_max
@@ -118,11 +117,22 @@ def collect_stats(collect_datetime, es=None):
              LEFT JOIN queries on stats.query_id=queries.id
              WHERE date > %s AND date < %s
     """
+    logger.debug("min is %s, max is %s, query is: %s" % (date_min, date_max, sql))
+
+    filename = "telem-output-structures-%s" % (datetime.now().isoformat())
+    output_fh = open(filename, 'w')
+    trace_fh = open("trace.log", 'w')
+
+    trace_fh.write("doingquery and looping through results...")
+
     for r in statsdbpool.fetchiter(sql, (date_min, date_max), cursor_factory=DictCursor):
         record_type = r["record_type"]
         stats_type = r["type"]
         query_hash = r["query_hash"]
         geocode = json.dumps(r["ip_geocode"], sort_keys=True)
+
+        trace_fh.write("record_type: [%s] stats_type: [%s] query_hash: [%s] geocode: %s \n\n" % (record_type, stats_type, query_hash, geocode))
+
         if record_type in record_types:
             if stats_type == "view":
                 for record_key in r["payload"]:
@@ -147,6 +157,9 @@ def collect_stats(collect_datetime, es=None):
                     recordset_stats[recordset_key][record_type][stats_type]["queries"][query_hash] += record_count
                     recordset_stats[recordset_key][record_type][stats_type]["geocodes"][geocode] += record_count
 
+    logger.info("finished with first loop through resultset for sums. Now beginning second loop.")
+    trace_fh.write("finished with first loop through resultset for sums. Now beginning second loop.")
+
     for recordset_key in recordset_stats:
         recordset_data = {
             "recordset_id": recordset_key,
@@ -155,6 +168,9 @@ def collect_stats(collect_datetime, es=None):
         for record_type in record_types:
             recordset_data[record_type] = {}
             for stat_type in stat_types:
+                log_line = "\n\n###############################\nbeginning second loop for record_type: [%s] stat_type: [%s] \n" % (record_type, stat_type)
+                trace_fh.write(log_line)
+
                 recordset_data[record_type][stat_type] = {
                     "count": recordset_stats[recordset_key][record_type][stat_type]["count"],
                     "total": recordset_stats[recordset_key][record_type][stat_type]["total"],
@@ -162,23 +178,45 @@ def collect_stats(collect_datetime, es=None):
                     "queries": [],
                     "geocodes": []
                 }
+
+                log_line = "recordset_data[record_type][stat_type] recordset_data[%s][%s]  is:\n%s" % (record_type, stat_type, recordset_data[record_type][stat_type])
+                trace_fh.write(log_line)
+
                 for k in recordset_stats[recordset_key][record_type][stat_type]["items"]:
+                    trace_fh.write("now looping through recordset_stats[recordset_key: %s][%s][%s][items] . \n" % (recordset_key, record_type, stat_type))
+                    trace_fh.write("k is: %s\n" %(k))
+                    trace_fh.write("count (recordset_stats[recordset_key][record_type][stat_type][items][k]) is: %s\n\n" % (recordset_stats[recordset_key][record_type][stat_type]["items"][k]))
+                    
                     recordset_data[record_type][stat_type]["items"].append({
                         "term": k,
                         "count": recordset_stats[recordset_key][record_type][stat_type]["items"][k]
                     })
                 for k in recordset_stats[recordset_key][record_type][stat_type]["queries"]:
+                    trace_fh.write("now looping through recordset_stats[recordset_key: %s][%s][%s][items] . \n" % (recordset_key, record_type, stat_type))
+                    trace_fh.write("k is: %s\n" %(k))
+                    trace_fh.write("count (recordset_stats[recordset_key][record_type][stat_type][queries][k]) is: %s\n\n" % (recordset_stats[recordset_key][record_type][stat_type]["queries"][k]))
+                    
+
                     recordset_data[record_type][stat_type]["queries"].append({
                         "term": k,
                         "count": recordset_stats[recordset_key][record_type][stat_type]["queries"][k]
                     })
                 for k in recordset_stats[recordset_key][record_type][stat_type]["geocodes"]:
+                    trace_fh.write("now looping through recordset_stats[recordset_key: %s][%s][%s][items] . \n" % (recordset_key, record_type, stat_type))
+                    trace_fh.write("json.loads(k) is: %s\n" %( json.loads(k)))
+                    trace_fh.write("count (recordset_stats[recordset_key][record_type][stat_type][geocodes][k]) is: %s\n\n" % (recordset_stats[recordset_key][record_type][stat_type]["geocodes"][k]))
+                    
                     recordset_data[record_type][stat_type]["geocodes"].append({
                         "geo": json.loads(k),
                         "count": recordset_stats[recordset_key][record_type][stat_type]["geocodes"][k]
                     })
-
-        es.index(index=indexName, doc_type=typeName, body=recordset_data)
+        trace_fh.write("writing recordset_data to ES: \n")
+        output_fh.write(json.dumps(recordset_data))
+        trace_fh.write(json.dumps(recordset_data))
+        
+        #es.index(index=indexName, doc_type=typeName, body=recordset_data)
+    
+    logger.info("end of script")
 
 
 def api_stats(es=None):

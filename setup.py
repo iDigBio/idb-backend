493205eec987444e92d565ce2c36e8f71016c573
Dan Stoner <danstoner@gmail.com>
2021-01-12 20:09:36 -0500

    fix syntax error

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         'coverage',
         'numpy',
         'scipy',
-        'gevent=1.3.0',
+        'gevent==1.3.0',
         'gipc>=0.6.0, <0.7.0',
         'unicodecsv>=0.14.1, < 0.15.0',
         'shapely',

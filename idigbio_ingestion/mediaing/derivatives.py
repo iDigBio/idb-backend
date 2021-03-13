1ae627291143423ab3445ec7f1078fece1e53ac9
Dan Stoner <danstoner@gmail.com>
2021-03-13 11:41:21 -0500

    Revert "python3 compatible imports, modules, methods"
    This reverts commit 161d90a369f50fef96eb091cc9dadd6b4f2b8090.

--- idigbio_ingestion/mediaing/derivatives.py
+++ idigbio_ingestion/mediaing/derivatives.py
@@ -1,10 +1,12 @@
 from __future__ import division, absolute_import
 from __future__ import print_function
+from future_builtins import map, filter
 
-from io import BytesIO
+import cStringIO
 
 from datetime import datetime
 from collections import Counter, namedtuple
+import itertools
 
 from gevent.pool import Pool
 from PIL import Image
@@ -85,7 +87,7 @@ def process_objects(objects):
         ci = get_keys(o)
         gr = generate_all(ci)
         return upload_all(gr)
-    results = pool.imap_unordered(one, filter(None, objects))
+    results = pool.imap_unordered(one, itertools.ifilter(None, objects))
     results = count_results(results, update_freq=100)
     etags = ((gr.etag,) for gr in results if gr)
     count = apidbpool.executemany(
@@ -149,7 +151,7 @@ get_store = memoized()(lambda: IDigBioStorage())
 
 def get_keys(obj):
     etag, bucket = obj.etag, obj.bucket
-    etag = str(etag)
+    etag = unicode(etag)
     s = get_store()
     bucketbase = u"idigbio-{0}-{1}".format(bucket, config.ENV)
     mediakey = s.get_key(etag, bucketbase)
@@ -233,7 +235,7 @@ def upload_item(item):
 def img_to_buffer(img, **kwargs):
     kwargs.setdefault('format', 'JPEG')
     kwargs.setdefault('quality', 95)
-    dervbuff = BytesIO()
+    dervbuff = cStringIO.StringIO()
     img.save(dervbuff, **kwargs)
     dervbuff.seek(0)
     return dervbuff

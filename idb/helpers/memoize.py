50da4b4c6b7a4115c1dee1de08afefe9aedfa66a
Dan Stoner <danstoner@gmail.com>
2021-03-13 11:49:07 -0500

    Revert "Python3 conversion - setup.py, json config load, and cPickle"
    This reverts commit 51b8cee904f239e7e0c2bf9c7574586849b6447e.

--- idb/helpers/memoize.py
+++ idb/helpers/memoize.py
@@ -2,7 +2,7 @@ from __future__ import division, absolute_import
 from __future__ import print_function
 
 from functools import wraps
-import pickle
+import cPickle
 from idb.helpers.logging import getLogger
 from atomicfile import AtomicFile
 
@@ -54,15 +54,15 @@ def _memoize_nargs_callthru(fn):
     return memo
 
 
-def _memoize_nargs_pickle(fn):
+def _memoize_nargs_cPickle(fn):
     "A memoizer for function w/ arbitrary length arguments."
     memory = {}
-    import pickle
+    import cPickle
 
     @wraps(fn)
     def memo(*args, **kwargs):
-        key = (pickle.dumps(args, pickle.HIGHEST_PROTOCOL),
-               pickle.dumps(kwargs, pickle.HIGHEST_PROTOCOL))
+        key = (cPickle.dumps(args, cPickle.HIGHEST_PROTOCOL),
+               cPickle.dumps(kwargs, cPickle.HIGHEST_PROTOCOL))
         try:
             v = memory[key]
         except KeyError:
@@ -72,20 +72,20 @@ def _memoize_nargs_pickle(fn):
     return memo
 
 
-def memoized(unhashable="pickle"):
+def memoized(unhashable="cPickle"):
     "Decorator to memoize a function."
     def getfn(fn):
         if fn.func_code.co_argcount == 0:
             memo = _memoize_0args(fn)
         elif unhashable == "call":
             memo = _memoize_nargs_callthru(fn)
-        elif unhashable == "pickle":
-            memo = _memoize_nargs_pickle(fn)
+        elif unhashable == "cPickle":
+            memo = _memoize_nargs_cPickle(fn)
         elif unhashable == "error":
             memo = _memoize_nargs_error(fn)
         else:
             raise ValueError(
-                "Uknown unhashable memoization strategy, expected {call, pickle, error}")
+                "Uknown unhashable memoization strategy, expected {call, cPickle, error}")
         if memo.__doc__:
             memo.__doc__ = memo.__doc__ + "\n\nThis function is memoized."
         else:
@@ -101,7 +101,7 @@ def filecached(filename, writeback=True):
     def writecache(value):
         logger.debug("Writing cache to %r", filename)
         with AtomicFile(filename, 'wb') as f:
-            pickle.dump(value, f)
+            cPickle.dump(value, f)
 
     def getfn(fn):
         @wraps(fn)
@@ -109,7 +109,7 @@ def filecached(filename, writeback=True):
             val = None
             try:
                 with open(filename, 'rb') as f:
-                    val = pickle.load(f)
+                    val = cPickle.load(f)
                     logger.debug("read cache from %r", filename)
             except (IOError, EOFError):
                 val = fn(*args, **kwargs)

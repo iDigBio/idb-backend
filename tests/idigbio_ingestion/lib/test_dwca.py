e00e93d96db262f0e298ba541a640d8fd158eb66
Dan Stoner <danstoner@gmail.com>
2021-04-02 15:08:07 -0400

    add stub tests for dwca.py

---
+++ tests/idigbio_ingestion/lib/test_dwca.py
@@ -0,0 +1,19 @@
+import pytest
+
+@pytest.mark.skip(reason="dwca.py needs refactoring")
+def test_dwca_schema_parse():
+    assert(False)
+
+@pytest.mark.skip(reason="dwca.py needs refactoring")
+def test_something_about_finding_extensions():
+    assert(False)
+
+@pytest.mark.skip(reason="dwca.py needs refactoring")
+def test_get_unescaped_fieldsTerminatedBy_from_filedict():
+    assert(False)
+
+@pytest.mark.skip(reason="dwca.py needs refactoring")
+def test_get_unescaped_fieldsEnclosedBy_from_filedict():
+    assert(False)
+
+

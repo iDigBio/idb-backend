2791bfe6d164594d21feaccf492f0b185248568a
Dan Stoner <danstoner@gmail.com>
2023-02-27 11:59:22 -0500

    add output for debugging docker pipeline commands

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -1,5 +1,6 @@
 # need to replace a forward slash for handling git flow feature/my-feature-name branches
 export TRAVIS_BRANCH_REPLACED=$(printf "%s" "$TRAVIS_BRANCH" | sed 's#/#-#g')
 
+echo "Inside docker-push script -- Preparing to tag and push..."
 docker tag $BUILDING $DOCKER_IMAGE:$TRAVIS_BRANCH_REPLACED
 docker push $DOCKER_IMAGE:$TRAVIS_BRANCH_REPLACED
